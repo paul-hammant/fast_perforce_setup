04d77a545031c2ac6eb9d018fd6ca5217d1f3e59
Paul Hammant <paul@hammant.org>
2015-01-31 08:34:47 -0500

    shape admin users client-spec

--- client/create-admin-account-and-more-security-stuff.sh
+++ client/create-admin-account-and-more-security-stuff.sh
@@ -29,3 +29,15 @@ p4 protect -i < .p4_protect
 rm .p4_protect
 
 p4 configure set security=3
+
+mkdir working_copy
+
+p4 client -o | sed '/^#/ d' > .p4_client
+yourClient=$(cat .p4_client | grep "^Client:" | cut -f 2)
+yourWorkingCopyDirectory=$(cat .p4_client | grep "^Root:" | cut -f 2)
+cat .p4_client | sed '/^Root:/ d' | sponge .p4_client
+echo "\n\nRoot: ${yourWorkingCopyDirectory}/wc" >>  .p4_client
+p4 client -i < .p4_client
+rm .p4_client
+
+echo "Your Perforce Client: ${yourClient}, and it's working copy dir: ${yourWorkingCopyDirectory}/wc"

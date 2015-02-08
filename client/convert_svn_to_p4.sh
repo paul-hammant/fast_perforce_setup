7b39164fc4e00332a8db9d6b3dffc8202a67d273
Paul Hammant <paul@hammant.org>
2015-02-08 06:46:10 -0500

    preserve double spaces in file names

--- client/convert_svn_to_p4.sh
+++ client/convert_svn_to_p4.sh
@@ -21,19 +21,20 @@ while ((i++)); read -r revision; do
     grep "^   [AMR]" svn_to_p4_revision.txt | grep "[AMR] ${prefix}/trunk/" | sed "s/^[ ]*//" | sed 's/(.*)//' | sed "s/[ ]*$//" | sed "s#[AMR] ${prefix}/trunk/##" > svn_to_p4_adds_mods.txt
     while read add_mod; do
         cmd="git add \"$add_mod\""
-        eval $cmd > svn_to_p4_gitadd_output.txt
+        eval "$cmd" > svn_to_p4_gitadd_output.txt
         cat svn_to_p4_gitadd_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} (add): fatal/" >> svn_to_p4_fatal_pathspecs.txt
 
     done <svn_to_p4_adds_mods.txt
     grep "^   D" svn_to_p4_revision.txt | grep "D ${prefix}/trunk" | sed "s#D ${prefix}/trunk/##" | sed "s/^[ ]*//" | sed "s/[ ]*$//" > svn_to_p4_deletes.txt
     while read del; do
         cmd="git rm -r \"$del\""
-        eval $cmd 2>&1 | sed "/^fatal/d" | sed "/^rm '/d" 
+        eval "$cmd" 2>&1 > svn_to_p4_rm_output.txt
+        cat svn_to_p4_rm_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} (rm): fatal/" >> svn_to_p4_fatal_pathspecs.txt  
     done <svn_to_p4_deletes.txt
     messageText=$(cat svn_to_p4_revision.txt | awk '/^$/ {do_print=1} do_print==1 {print} NF==3 {do_print=0}' | sed '/------/d') | sed 's/\"/\\\"/g'
     headers="Svn Rev: ${rev}."
     cmd="git commit --quiet --author \"${author} <${author}@unsure>\" --date \"${date}\" -m \"${headers}${messageText}\""
-    eval $cmd | sed '/^ create mode/d' > svn_to_p4_commit_output.txt
+    eval "$cmd" | sed '/^ create mode/d' > svn_to_p4_commit_output.txt
 
     #if [ $rev -eq 65 ]; then
     #   break
@@ -44,7 +45,7 @@ while ((i++)); read -r revision; do
        break
     fi
 
-    rm svn_to_p4_revision.txt svn_to_p4_deletes.txt svn_to_p4_commit_output.txt svn_to_p4_up.txt svn_to_p4_adds_mods.txt 2>&1 | sed '/No such file or directory/d'
+    rm svn_to_p4_revision.txt svn_to_p4_deletes.txt svn_to_p4_commit_output.txt svn_to_p4_up.txt svn_to_p4_adds_mods.txt svn_to_p4_rm_output.txt 2>&1 | sed '/No such file or directory/d'
     
     if [[ $(( rev % 4000 )) == 0 ]]; then
         echo "Git repack & gc"..

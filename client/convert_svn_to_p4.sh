cf008d7dd75ced5a5db8e31e7c5cd6063ec6b983
Paul Hammant <paul@hammant.org>
2015-02-09 21:48:22 -0500

    keep copies of commits to git

--- client/convert_svn_to_p4.sh
+++ client/convert_svn_to_p4.sh
@@ -6,6 +6,12 @@ rm svn_to_p4_fatal_pathspecs.txt 2>&1 | sed "/No such file or directory/d"
 
 prefix=$(svn info | grep "^Relative URL:" | sed 's/Relative URL: ^//' | sed 's#/trunk##')
 
+echo -e ".svn\nsvn_to_p4_commits\nsvn_to_p4_adds_mods.txt\nsvn_to_p4_deletes.txt\nsvn_to_p4_fatal_pathspecs.txt\nsvn_to_p4_gitadd_output.txt\nsvn_to_p4_revision.txt\nsvn_to_p4_revisions.txt\nsvn_to_p4_rm_output.txt\nsvn_to_p4_up.txt" > .gitignore
+git add .gitignore
+git commit -m "ignore some temp files used in svn conversion process"
+
+mkdir svn_to_p4_commits
+
 while ((i++)); read -r revision; do
 
     trap "echo Exited!; exit;" SIGINT SIGTERM
@@ -22,30 +28,19 @@ while ((i++)); read -r revision; do
     while read add_mod; do
         cmd="git add \"$add_mod\""
         eval "$cmd" > svn_to_p4_gitadd_output.txt
-        cat svn_to_p4_gitadd_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} (add): fatal/" >> svn_to_p4_fatal_pathspecs.txt
+        cat svn_to_p4_gitadd_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} add: fatal/" >> svn_to_p4_fatal_pathspecs.txt
 
     done <svn_to_p4_adds_mods.txt
     grep "^   D" svn_to_p4_revision.txt | grep "D ${prefix}/trunk" | sed "s#D ${prefix}/trunk/##" | sed "s/^[ ]*//" | sed "s/[ ]*$//" > svn_to_p4_deletes.txt
     while read del; do
         cmd="git rm -r \"$del\""
         eval "$cmd" 2>&1 > svn_to_p4_rm_output.txt
-        cat svn_to_p4_rm_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} (rm): fatal/" >> svn_to_p4_fatal_pathspecs.txt  
+        cat svn_to_p4_rm_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} rm: fatal/" >> svn_to_p4_fatal_pathspecs.txt  
     done <svn_to_p4_deletes.txt
     messageText=$(cat svn_to_p4_revision.txt | awk '/^$/ {do_print=1} do_print==1 {print} NF==3 {do_print=0}' | sed '/------/d') | sed 's/\"/\\\"/g'
     headers="Svn Rev: ${rev}."
-    cmd="git commit --quiet --author \"${author} <${author}@unsure>\" --date \"${date}\" -m \"${headers}${messageText}\""
-    eval "$cmd" | sed '/^ create mode/d' > svn_to_p4_commit_output.txt
-
-    #if [ $rev -eq 65 ]; then
-    #   break
-    #fi
-
-    if grep -q "Changes not staged for commit" svn_to_p4_commit_output.txt; then
-       echo "Changes not staged for commit"
-       break
-    fi
-
-    # rm svn_to_p4_revision.txt svn_to_p4_deletes.txt svn_to_p4_commit_output.txt svn_to_p4_up.txt svn_to_p4_adds_mods.txt svn_to_p4_rm_output.txt 2>&1 | sed '/No such file or directory/d'
+    cmd="git commit --author \"${author} <${author}@unsure>\" --date \"${date}\" -m \"${headers}${messageText}\""
+    eval "$cmd" > svn_to_p4_commits/"${rev}".txt
     
     if [[ $(( rev % 4000 )) == 0 ]]; then
         echo "Git repack & gc"..

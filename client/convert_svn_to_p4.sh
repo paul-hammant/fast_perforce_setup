a489c5a92bf1a5aceab8a40e0f489487186c72e9
Paul Hammant <paul@hammant.org>
2015-02-10 06:52:00 -0500

    streamlined some

--- client/convert_svn_to_p4.sh
+++ client/convert_svn_to_p4.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-svn log --xml | xmlstarlet sel -t -m 'log/logentry' -v "concat(@revision, '|', author, '|', date )" -n | sort -n > svn_to_p4_revisions.txt
+if  [ ! -d .git ]; then echo "no .git folder - do 'git init'"; exit 10; fi
+if  [ ! -d .svn ]; then echo "no .svn folder - checkout the trunk of some subversion repo"; exit 10; fi
 
-rm svn_to_p4_fatal_pathspecs.txt 2>&1 | sed "/No such file or directory/d"
+svn log --xml | xmlstarlet sel -t -m 'log/logentry' -v "concat(@revision, '|', author, '|', date )" -n | sort -n > svn_to_p4_revisions.txt
 
 prefix=$(svn info | grep "^Relative URL:" | sed 's/Relative URL: ^//' | sed 's#/trunk##')
 
-echo -e ".svn\nsvn_to_p4_commits\nsvn_to_p4_adds_mods.txt\nsvn_to_p4_deletes.txt\nsvn_to_p4_fatal_pathspecs.txt\nsvn_to_p4_gitadd_output.txt\nsvn_to_p4_revision.txt\nsvn_to_p4_revisions.txt\nsvn_to_p4_rm_output.txt\nsvn_to_p4_up.txt" > .gitignore
-git add .gitignore
-git commit -m "ignore some temp files used in svn conversion process"
+echo -e ".svn\nsvn_to_p4_commits\nsvn_to_p4_adds_mods.txt\nsvn_to_p4_deletes.txt\nsvn_to_p4_gitadd_output.txt\nsvn_to_p4_revision.txt\nsvn_to_p4_revisions.txt\nsvn_to_p4_rm_output.txt\nsvn_to_p4_up.txt" > .gitignore
+git add .gitignore > /dev/null
+git commit -m "ignore some temp files used in svn conversion process" > /dev/null
 
-mkdir svn_to_p4_commits
+mkdir svn_to_p4_commits 2>&1 > /dev/null
 
 while ((i++)); read -r revision; do
 
@@ -27,16 +28,15 @@ while ((i++)); read -r revision; do
     grep "^   [AMR]" svn_to_p4_revision.txt | grep "[AMR] ${prefix}/trunk/" | sed "s/^[ ]*//" | sed 's/(.*)//' | sed "s/[ ]*$//" | sed "s#[AMR] ${prefix}/trunk/##" > svn_to_p4_adds_mods.txt
     while read add_mod; do
         cmd="git add \"$add_mod\""
-        eval "$cmd" > svn_to_p4_gitadd_output.txt
-        cat svn_to_p4_gitadd_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} add: fatal/" >> svn_to_p4_fatal_pathspecs.txt
-
+        eval "$cmd" 
     done <svn_to_p4_adds_mods.txt
     grep "^   D" svn_to_p4_revision.txt | grep "D ${prefix}/trunk" | sed "s#D ${prefix}/trunk/##" | sed "s/^[ ]*//" | sed "s/[ ]*$//" > svn_to_p4_deletes.txt
     while read del; do
-        cmd="git rm -r \"$del\""
+        cmd="git rm -q --ignore-unmatch -r \"$del\""
         eval "$cmd" 2>&1 > svn_to_p4_rm_output.txt
-        cat svn_to_p4_rm_output.txt | grep "fatal: pathspec" | sed "s/fatal/${rev} rm: fatal/" >> svn_to_p4_fatal_pathspecs.txt  
+        cat svn_to_p4_rm_output.txt 2>&1 | sed "/^fatal: pathspec.*/d" 
     done <svn_to_p4_deletes.txt
+
     messageText=$(cat svn_to_p4_revision.txt | awk '/^$/ {do_print=1} do_print==1 {print} NF==3 {do_print=0}' | sed '/------/d') | sed 's/\"/\\\"/g'
     headers="Svn Rev: ${rev}."
     cmd="git commit --author \"${author} <${author}@unsure>\" --date \"${date}\" -m \"${headers}${messageText}\""
@@ -53,6 +53,4 @@ done < svn_to_p4_revisions.txt
 time git repack 
 time git gc
 
-echo "FATAL PATHSPECS (if any)..."
-cat svn_to_p4_fatal_pathspecs.txt
 echo "ALL DONE."

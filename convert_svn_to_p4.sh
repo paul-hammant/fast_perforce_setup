206abd38199e6ad0c26758b84e312510cb2f63bd
Paul Hammant <paul@hammant.org>
2015-02-22 15:27:22 -0500

    switch to resolve way of adding items to P4

--- convert_svn_to_p4.sh
+++ convert_svn_to_p4.sh
@@ -1,42 +1,12 @@
 #!/bin/bash
 
-p4_add_file(){
-    if [ -f "$1" ]; then
-        p4 add "$1" 2>&1 | sed '/opened for add$/d'
-    fi
-}
-p4_add_dir(){
-    if [ -d "$1" ]; then
-        p4 add "$1/..." 2>&1 | sed '/opened for add$/d' | sed "/can\'t add existing file/d"
-    fi
-}
-p4_del_file(){
-    if [ -f "$1" ]; then
-        p4 delete "$1" 2>&1 | sed '/opened for delete$/d'
-    fi
-}
-p4_del_dir(){
-    if [ -d "$1" ]; then
-        p4 delete "$1/..." 2>&1 | sed '/opened for delete$/d'
-    fi
-}
-p4_edit_file(){
-    # echo -n "E: $1"
-    p4 edit "$1" 2>&1 | sed '/opened for edit$/d'
-}
-export -f p4_add_file
-export -f p4_add_dir
-export -f p4_del_file
-export -f p4_del_dir
-export -f p4_edit_file
-
 # if  [ ! -d .p4root ]; then echo "no .p4root folder - do 'p4 init'"; exit 10; fi
 if  [ ! -d .svn ]; then echo "no .svn folder - checkout the trunk of some subversion repo"; exit 10; fi
-[ ! -d svn_to_p4_commits ] && mkdir svn_to_p4_commits
+[ ! -d ../svn_to_p4_commits ] && mkdir ../svn_to_p4_commits
 # p4 edit .p4ignore 
 # echo -e ".svn\nsvn_to_p4_commits\nsvn_to_p4_revision.txt\nsvn_to_p4_revisions.txt" >> .p4ignore
 
-svn log | grep '^r[0-9]* ' | cut -d' ' -f 1 | cut -d'r' -f 2 | sort -n > svn_to_p4_revisions.txt
+svn log | grep '^r[0-9]* ' | cut -d' ' -f 1 | cut -d'r' -f 2 | sort -n > ../svn_to_p4_revisions.txt
 prefix=$(svn info | grep "^Relative URL:" | sed 's/Relative URL: ^//' | sed 's#/trunk##')
 
 while ((i++)); read -r rev; do
@@ -44,34 +14,25 @@ while ((i++)); read -r rev; do
 
     # if [ "$rev" != "366" ]; then continue; fi
 
-    svn log -v -r $rev > svn_to_p4_revision.txt
+    svn log -v -r $rev > ../svn_to_p4_revision.txt
 
-    revisionLine=$(cat svn_to_p4_revision.txt | grep '^r[0-9]* ')
+    revisionLine=$(cat ../svn_to_p4_revision.txt | grep '^r[0-9]* ')
     author=$(echo $revisionLine | cut -d'|' -f2 | sed 's/(no author)/none/' | cut -d' ' -f2 | sed "s/^$/none/")
     date=$(echo $revisionLine | cut -d'|' -f3 | cut -d'(' -f1)
-    messageText=$(cat svn_to_p4_revision.txt | awk '/^$/ {do_print=1} do_print==1 {print} NF==3 {do_print=0}' | sed '/------/d' | sed 's/\"/\\\"/g') 
-
-    cat svn_to_p4_revision.txt | sed "s/^ *//" | sed 's/(.*)$//' | sed "s/ *$//" | grep "${prefix}/trunk/" | sed "s#${prefix}/trunk/##" | sponge svn_to_p4_revision.txt
-
-    grep "^[MR]" svn_to_p4_revision.txt | cut -d' ' -f 2-99 | xargs -I {} -n 1 bash -c "p4_edit_file \"{}\""
-    grep "^D" svn_to_p4_revision.txt | cut -d' ' -f 2-99 | xargs -I {} -n 1 bash -c "p4_del_file \"{}\""
-    grep "^D" svn_to_p4_revision.txt | cut -d' ' -f 2-99 | xargs -I {} -n 1 bash -c "p4_del_dir \"{}\""
+    messageText=$(cat ../svn_to_p4_revision.txt | awk '/^$/ {do_print=1} do_print==1 {print} NF==3 {do_print=0}' | sed '/------/d' | sed 's/\"/\\\"/g') 
 
     svn up --force -r $rev | sed '/^At revision/d' | sed '/^Updating /d' | sed '/^[AUD]  /d' | sed '/^ U/d' | sed '/^Updated to/d' | sed '/^Restored /d'
+    p4 reconcile  2>&1 | sed '/opened for add$/d' | sed '/opened for move$/d' | sed '/opened for delete$/d' | sed '/opened for edit$/d' | sed '/opened for move\/add$/d' | sed '/opened for move\/delete$/d' | sed '/^\.\.\. moved from/d' > ../svn_to_p4_reconcile_output
 
-    grep "^A" svn_to_p4_revision.txt | cut -d' ' -f 2-99 | xargs -I {} -n 1 bash -c "p4_add_file \"{}\""
-    grep "^A" svn_to_p4_revision.txt | cut -d' ' -f 2-99 | xargs -I {} -n 1 bash -c "p4_add_dir \"{}\""
-
-    p4 submit -d "Svn Rev: ${rev}.${messageText}" > svn_to_p4_commits/"${rev}".txt 2>&1
-    if ! grep -q foo <<<"No files to submit from the default changelist."; then
+    if ! grep -Fq "No file(s) to reconcile." ../svn_to_p4_reconcile_output; then
+        p4 submit -d "Svn Rev: ${rev}.${messageText}" > ../svn_to_p4_commits/"${rev}".txt 2>&1
         p4Rev=$(p4 changes -m 1 | cut -d' ' -f2)
         # p4 change -f -U $author $p4Rev
         # --date "\"${date}\"" 
+        echo "Svn revision ${rev} on $(echo $date | cut -d' ' -f 1,2) is Perforce # $p4Rev"    
     fi
-
-    echo "Svn revision ${rev} on $(echo $date | cut -d' ' -f 1,2) is Perforce # $p4Rev"    
 
     # if [ "$rev" == "380" ]; then break; fi
 
-done < svn_to_p4_revisions.txt
+done < ../svn_to_p4_revisions.txt
 echo "ALL DONE WITH A P4 REPO SIZE OF $(du -h -d 0 .p4root | cut -f1)."
